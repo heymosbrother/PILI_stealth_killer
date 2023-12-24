9aa86bb55d2408d143deba889233f5ff43aa6be8
gl4jo6mp6 <gl4jo6mp6@gmail.com>
2023-12-24 09:39:54 +0000

    1224 pili_container add send to uart inside user service

--- pili_stealth_ws/src/userPkg/src/command_service_server.cpp
+++ pili_stealth_ws/src/userPkg/src/command_service_server.cpp
@@ -1,9 +1,9 @@
 //command_service_server.cpp
 #include "rclcpp/rclcpp.hpp"
 #include "more_interfaces/srv/command_service.hpp"
-/*#include <wiringPi.h>
+#include <wiringPi.h>
 #include <wiringSerial.h>
-*/
+
 class CommandServiceServer : public rclcpp::Node
 {
 public:
@@ -40,60 +40,15 @@ private:
         response->allcmd[6] = (request->tm3 == 255) ? 'n' : static_cast<uint8_t>(request->tm3);
         response->allcmd[7] = (request->ts == 255) ? 'n' : static_cast<uint8_t>(request->ts);
         
-        uint8_t sendcmd[8] = {response->allcmd[0],response->allcmd[1],response->allcmd[2],response->allcmd[3],response->allcmd[4],response->allcmd[5],response->allcmd[6],response->allcmd[7]};
+        uint8_t sendcmd[8] = {request->mode,request->vm1,request->tm1,request->vm2,request->tm2,request->vm3,request->tm3,request->ts};
         /*RCLCPP_INFO(get_logger(), "Received request ");
         for (const auto& element : response->allcmd) {
         RCLCPP_INFO(get_logger(), "%u", element);
         }*/
         RCLCPP_INFO(get_logger(), "Received request %.*s", static_cast<int>(response->allcmd.size()), response->allcmd.data());
-
-    }
-
-
-    rclcpp::Service<more_interfaces::srv::CommandService>::SharedPtr service_;
-};
-int main(int argc, char **argv)
-{
-    rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<CommandServiceServer>());
-    rclcpp::shutdown();
-    return 0;
-}
- /*
- #include "rclcpp/rclcpp.hpp"
-#include "more_interfaces/srv/command_service.hpp"
-#include <wiringPi.h>
-#include <wiringSerial.h>
-
-class CommandServiceServer : public rclcpp::Node
-{
-public:
-    explicit CommandServiceServer()
-        : Node("command_service_server")
-    {
-        // Create the service
-        service_ = create_service<more_interfaces::srv::CommandService>(
-            "command_service", std::bind(&CommandServiceServer::handle_request, this, std::placeholders::_1, std::placeholders::_2));
-    }
-
-private:
-    void handle_request(const std::shared_ptr<more_interfaces::srv::CommandService::Request> request,
-                        std::shared_ptr<more_interfaces::srv::CommandService::Response> response)
-    {
-        response->allcmd[0] = (request->mode == 255) ? 'n' : static_cast<uint8_t>(request->mode);
-        response->allcmd[1] = (request->vm1 == 255) ? 'n' : static_cast<uint8_t>(request->vm1);
-        response->allcmd[2] = (request->tm1 == 255) ? 'n' : static_cast<uint8_t>(request->tm1);
-        response->allcmd[3] = (request->vm2 == 255) ? 'n' : static_cast<uint8_t>(request->vm2);
-        response->allcmd[4] = (request->tm2 == 255) ? 'n' : static_cast<uint8_t>(request->tm2);
-        response->allcmd[5] = (request->vm3 == 255) ? 'n' : static_cast<uint8_t>(request->vm3);
-        response->allcmd[6] = (request->tm3 == 255) ? 'n' : static_cast<uint8_t>(request->tm3);
-        response->allcmd[7] = (request->ts == 255) ? 'n' : static_cast<uint8_t>(request->ts);
-
-        uint8_t sendcmd[8] = {request->mode, request->vm1, request->tm1, request->vm2, request->tm2, request->vm3, request->tm3, request->ts};
-        RCLCPP_INFO(get_logger(), "Received request %.*s", response->allcmd.size(), response->allcmd.data());
-
+        
         // Send the sendcmd array via UART
-        int uart_serial = serialOpen("/dev/ttyS0", 115200);  // Adjust the serial port and baud rate as needed
+        int uart_serial = serialOpen("/dev/ttyS0", 115200);  // Adjust the serial port and baud rate
         if (uart_serial != -1) {
             serialPuts(uart_serial, reinterpret_cast<char*>(sendcmd));
             serialClose(uart_serial);
@@ -102,21 +57,18 @@ private:
         }
     }
 
+
     rclcpp::Service<more_interfaces::srv::CommandService>::SharedPtr service_;
 };
-
 int main(int argc, char **argv)
 {
     rclcpp::init(argc, argv);
-
     // Initialize WiringPi
     if (wiringPiSetup() == -1) {
         RCLCPP_ERROR(rclcpp::get_logger("main"), "Unable to initialize WiringPi");
         return -1;
     }
-
     rclcpp::spin(std::make_shared<CommandServiceServer>());
     rclcpp::shutdown();
     return 0;
 }
-*/
